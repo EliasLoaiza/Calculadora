4dcfda7033a77028a9cb124e8011b744919d4065
EliasLoaiza <eliasloaizarincon@gmail.com>
2023-07-14 14:38:23 -0400

    Connect Logic and Buttoms with Screen

--- src/Componets/Base/base.js
+++ src/Componets/Base/base.js
@@ -3,11 +3,12 @@ import { Buttoms } from '../Buttoms/Buttoms';
 import { Screen } from '../Screen/Screen';
 import React from 'react';
 function Base(){
-    const [datosValue, setdatosValue] = React.useState(0);
+    const [datosValue, setdatosValue] = React.useState('');
+    console.log(datosValue);
     return (
             <div className='base'>
                 <Screen datosValue={datosValue} />
-                <Buttoms setdatosValue={setdatosValue}/>
+                <Buttoms setdatosValue={setdatosValue} datosValue={datosValue}/>
             </div>
     );
 }
--- src/Componets/Buttoms/Buttoms.js
+++ src/Componets/Buttoms/Buttoms.js
@@ -1,53 +1,73 @@
 
 import React from 'react';
 import './Buttoms.css'
-function Buttoms({setdatosValue}){
-    var operation = "";
-    function totalOperation(data) {
-        if (data.includes('+')){
-            let array = data.split('=');
+function Buttoms({setdatosValue , datosValue}){
+    const operation = {
+        solved: false,
+        data1: "",
+        data2: "",
+        complete: "",
+    };
+    console.log(operation.solved);
+    console.log(operation.complete);
+
+    function totalOperation(operation) {
+        if (operation.complete.includes('+')){
+            let array = operation.complete.split('=');
             let array2 = array[0].split('+');
-            let data1 = array2[0];
-            let data2 = array2[1];   
-            let result = Number(data1) + Number(data2);  
-            console.log(result);
-        } else  if (data.includes('-')){
-            let array = data.split('=');
+            operation.data1 = array2[0];
+            operation.data2 = array2[1];
+            let result = Number(operation.data1) + Number(operation.data2);
+            setdatosValue(result);
+            operation.complete = "";
+            operation.solved = true;   
+        } else  if (operation.includes('-')){
+            let array = operation.split('=');
             let array2 = array[0].split('-');
             let data1 = array2[0];
-            let data2 = array2[1];   
-            let result = Number(data1) - Number(data2);  
-            console.log(result);
-        } else  if (data.includes('/')){
-            let array = data.split('=');
+            let data2 = array2[1];
+            let result = Number(data1) - Number(data2);
+            setdatosValue(result);
+            operation.complete = "";
+        } else  if (operation.includes('/')){
+            let array = operation.split('=');
             let array2 = array[0].split('/');
             let data1 = array2[0];
-            let data2 = array2[1];   
-            let result = Number(data1) / Number(data2);  
-            console.log(result);
-        } else  if (data.includes('*')){
-            let array = data.split('=');
-            let array2 = array[0].split('8');
+            let data2 = array2[1];
+            let result = Number(data1) / Number(data2);
+            setdatosValue(result);  
+            operation.complete = "";
+        } else  if (operation.includes('*')){
+            let array = operation.split('=');
+            let array2 = array[0].split('*');
             let data1 = array2[0];
-            let data2 = array2[1];   
-            let result = Number(data1) * Number(data2);  
-            console.log(result);
+            let data2 = array2[1];
+            let result = Number(data1) * Number(data2);
+            setdatosValue(result);
+            operation.complete = "";
         }
     }
     function saveDato(data){
-        operation = operation + data;
-        console.log(operation);
-        if(operation.includes("=")){
-            totalOperation(operation)
+        if(operation.solved){
+            console.log('true')
+            setdatosValue("");
+            operation.solved = false;
+            console.log(operation.solved)
+        }else{
+            console.log('False')
+            operation.complete = datosValue + data;
+            setdatosValue(operation.complete)
+            if(operation.complete.includes("=")){
+            totalOperation(operation);
+            }
         }
- 
     }
     return(
         <div className='buttoms'>
                 <button className='buttom' onClick={() => saveDato("1")}>1</button>
                 <button className='buttom' onClick={() => saveDato("2")}>2</button>
                 <button className='buttom' onClick={() => saveDato("3")}>3</button>
-                <button className='butto m' onClick={() => saveDato("4")}>4</button>
+                <button className='buttom' onClick={() => saveDato("4")}>4</button>
                 <button className='buttom' onClick={() => saveDato("5")}>5</button>
                 <button className='buttom' onClick={() => saveDato("6")}>6</button>
                 <button className='buttom' onClick={() => saveDato("7")}>7</button>
@@ -58,7 +78,7 @@ function Buttoms({setdatosValue}){
                 <button className='buttom' onClick={() => saveDato(".")}>.</button>
                 <button className='operation' onClick={() => saveDato("+")}>+</button>
                 <button className='operation' onClick={() => saveDato("-")}>-</button>
-                <button className='operation'onClick={() => saveDato("*")}>x</button>
+                <button className='operation' onClick={() => saveDato("*")}>x</button>
                 <button className='operation' onClick={() => saveDato("/")}>/</button>
                 <button className='operation' onClick={() => saveDato("=")}>=</button>
          </div>
